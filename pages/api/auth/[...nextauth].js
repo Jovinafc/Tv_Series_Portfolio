298f6f8be5c3731fc29f13a727668084d3ee7455
unknown <dsouza.jovin65@gmail.com>
2021-07-08 23:04:31 +0530

    test env key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId:
+        '635752708519-bfi2ep692jd8u2dhnqupvu1vk0fes3fj.apps.googleusercontent.com',
+      clientSecret: 'wwJ29ZseXCOYf20X2qNaKJfu',
     }),
   ],
 });
